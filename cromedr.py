fb4c23fbe8254807026af7bee859dbf37a7e3990
nownuu <nownuu98@naver.com>
2022-10-01 21:43:08 +0900

    1001_웹크롤링 : 페이지 오픈 (수정)

--- cromedr.py
+++
@@ -1,8 +0,0 @@
-# 웹크롤링 시작
-# webdriver selenium 으로 네이버 오픈해보기
-
-
-from selenium import webdriver
-driver = webdriver.Chrome() # 같은 위치라서 () / 다른 위치면 (" 주소 ")로 입력하기
-url = 'https://www.naver.com'
-driver.get(url)
---
+++ page_login.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+url = 'https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com'
+driver.get(url)
